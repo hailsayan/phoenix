2bc32855e9ff7f7ad533e0da17fcc26b45b823c7
Psyon <sayan.h2020@gmail.com>
2024-11-25 19:59:11 +0330

    add pointer & struct

--- ep03/main.go
+++ ep03/main.go
@@ -2,53 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-// type DNF struct {
-// 	version string
-// }
-
-// func (dnf *DNF) update(pkg string) {
-// 	fmt.Printf("updating package %s\n", pkg)
-// }
-
-type geometry interface {
-	area() float64
+type DNF struct {
+	version string
 }
 
-type rectangle struct {
-	width, height float64
-}
-
-func (r rectangle) area() float64 {
-	return r.height * r.width
-}
-
-type circle struct {
-	radius float64
-}
-
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func printArea(g geometry) {
-	fmt.Printf("the geometry area is %f\n", g.area())
+func (dnf *DNF) update(pkg string) {
+	fmt.Printf("updating package %s\n", pkg)
 }
 
 func main() {
 
-	r1 := &rectangle{width: 5, height: 3}
-	c1 := &circle{radius: 4}
+	fedora := &DNF{version: "41"}
+	fmt.Printf("%p\n", fedora)
 
-	printArea(r1)
-	printArea(c1)
+	fmt.Println(fedora.version)
 
-	// fedora := DNF{version: "41"}
-	// fmt.Printf("%p\n, &fedora")
-
-	// fmt.Println(fedora.version)
-
-	// fedora.update("curl")
+	fedora.update("curl")
 }
